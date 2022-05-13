282e3e32ca2000ba7e5ecddc1d0f818689a56886
yong <yongpinbong@gmail.com>
2022-05-13 08:25:24 +0800

    initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about flex justify-center outline-dotted mt-1 items-center">
+    <h1 class="text-green-300">This is an about page with tailwind class</h1>
+  </div>
+</template>
+
+<style>
+</style>
